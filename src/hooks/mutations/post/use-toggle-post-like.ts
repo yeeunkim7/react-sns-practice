513bf97cc1bef405cfded85f8358baab831ee6c6
yeeunkim7 <tyeole7172@naver.com>
2026-01-11 20:43:05 +0900

    Feat: 좋아요 기능에 낙관적 업데이트 적용 및 UI 즉시 반영 구현

--- src/hooks/mutations/post/use-toggle-post-like.ts
+++ src/hooks/mutations/post/use-toggle-post-like.ts
@@ -1,14 +1,45 @@
 import { togglePostLike } from "@/api/post";
-import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import { type Post, type UseMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function useTogglePostLike(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: togglePostLike,
+    onMutate: async ({ postId }) => {
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.post.byId(postId),
+      });
+
+      const prevPost = queryClient.getQueryData<Post>(
+        QUERY_KEYS.post.byId(postId),
+      );
+
+      queryClient.setQueryData<Post>(QUERY_KEYS.post.byId(postId), (post) => {
+        if (!post) throw new Error("포스트가 존재하지 않습니다.");
+        return {
+          ...post,
+          isLiked: !post.isLiked,
+          like_count: post.isLiked ? post.like_count - 1 : post.like_count + 1,
+        };
+      });
+
+      return {
+        prevPost,
+      };
+    },
     onSuccess: () => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
     },
-    onError: (error) => {
+    onError: (error, _, context) => {
+      if (context && context.prevPost) {
+        queryClient.setQueryData(
+          QUERY_KEYS.post.byId(context.prevPost.id),
+          context.prevPost,
+        );
+      }
       if (callbacks?.onError) callbacks.onError(error);
     },
   });
