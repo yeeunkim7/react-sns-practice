6414be0f911deb91c72593eb1238c009ac64e8c6
yeeunkim7 <tyeole7172@naver.com>
2026-01-20 21:49:56 +0900

    feat: implement profile detail page with feed and profile info

--- src/pages/profile-detail-page.tsx
+++ src/pages/profile-detail-page.tsx
@@ -1,3 +1,25 @@
+import PostFeed from "@/components/post/post-feed";
+import ProfileInfo from "@/components/profile/profile-info";
+import { useEffect } from "react";
+import { Navigate, useParams } from "react-router";
+
 export default function ProfileDetailPage() {
-  return <div>ProfileDetailPage</div>;
+  const params = useParams();
+  const userId = params.userId;
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+    });
+  }, []);
+
+  if (!userId) return <Navigate to={"/"} replace />;
+
+  return (
+    <div className="flex flex-col gap-10">
+      <ProfileInfo userId={userId} />
+      <div className="border-b"></div>
+      <PostFeed authorId={userId} />
+    </div>
+  );
 }
