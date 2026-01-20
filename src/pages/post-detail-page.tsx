7e5e347f5dfde52ee2e1cb60a5bf486aaafb6e63
yeeunkim7 <tyeole7172@naver.com>
2026-01-20 21:47:45 +0900

    feat: implement post detail page with comments

--- src/pages/post-detail-page.tsx
+++ src/pages/post-detail-page.tsx
@@ -1,3 +1,20 @@
+import CommentEditor from "@/components/comment/comment-editor";
+import CommentList from "@/components/comment/comment-list";
+import PostItem from "@/components/post/post-item";
+import { Navigate, useParams } from "react-router";
+
 export default function PostDetailPage() {
-  return <div>PostDetailPage</div>;
+  const params = useParams();
+  const postId = params.postId;
+
+  if (!postId) return <Navigate to={"/"} />;
+
+  return (
+    <div className="flex flex-col gap-5">
+      <PostItem postId={Number(postId)} type={"DETAIL"} />
+      <div className="text-xl font-bold">댓글</div>
+      <CommentEditor />
+      <CommentList />
+    </div>
+  );
 }
