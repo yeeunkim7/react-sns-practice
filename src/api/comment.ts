39315b6146d35cd3dfeeb5b7e2c0383b1e93bbb0
yeeunkim7 <tyeole7172@naver.com>
2026-01-27 21:43:54 +0900

    Fix : 댓글 작성 시 작성한 날에 맞게 정렬되도록 수정

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -5,7 +5,7 @@ export async function fetchComments(postId: number) {
     .from("comment")
     .select("*, author: profile!author_id (*)")
     .eq("post_id", postId)
-    .order("created_at", { ascending: false });
+    .order("created_at", { ascending: true });
 
   if (error) throw error;
   return data;
@@ -14,15 +14,18 @@ export async function fetchComments(postId: number) {
 export async function createComment({
   postId,
   content,
+  parentCommentId,
 }: {
   postId: number;
   content: string;
+  parentCommentId?: number;
 }) {
   const { data, error } = await supabase
     .from("comment")
     .insert({
       post_id: postId,
       content: content,
+      parent_comment_id: parentCommentId,
     })
     .select()
     .single();
--- src/hooks/mutations/comment/use-create-comment.ts
+++ src/hooks/mutations/comment/use-create-comment.ts
@@ -24,7 +24,7 @@ export function useCreateComment(callbacks?: UseMutationCallback) {
           if (!profile)
             throw new Error("사용자의 프로필 정보를 찾을 수 없습니다.");
 
-          return [{ ...newComment, author: profile }, ...comments];
+          return [...comments, { ...newComment, author: profile }];
         },
       );
     },
