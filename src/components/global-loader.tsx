75343eed59742ab27c21e58ecf628df8ba94f39e
yeeunkim7 <tyeole7172@naver.com>
2026-01-28 20:56:50 +0900

    fix : 로고 이미지를 인스타그램 로고로 수정

--- src/components/global-loader.tsx
+++ src/components/global-loader.tsx
@@ -1,11 +1,12 @@
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo2.png";
 
 export default function GlobalLoader() {
   return (
     <div className="bg-muted flex h-[100vh] w-[100vw] flex-col items-center justify-center">
       <div className="mb-15 flex animate-bounce items-center gap-4">
-        <img src={logo} alt="한입 로그 서비스의 로고" className="w-10" />
-        <div className="text-2xl font-bold">한입 로그</div>
+        <img src={logo} alt="react-sns-practice 로고" className="w-10"/>
+      <div className="text-2xl font-bold">react-sns-practice</div>
+
       </div>
     </div>
   );
--- src/components/layout/global-layout.tsx
+++ src/components/layout/global-layout.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from "react-router";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo2.png";
 import defaultAvatar from "@/assets/default-avatar.png";
 import { SunIcon } from "lucide-react";
 import ProfileButton from "@/components/layout/header/profile-button";
@@ -10,25 +10,38 @@ export default function GlobalLayout() {
     <div className="flex min-h-[100vh] flex-col">
       <header className="h-15 border-b">
         <div className="m-auto flex h-full w-full max-w-175 justify-between px-4">
-          <Link to={"/"} className="flex items-center gap-2">
+          <Link to="/" className="flex items-center gap-2">
             <img
-              className="h-5"
               src={logo}
-              alt="한입 로그의 로고, 메세지 말풍선을 형상화한 모양이다"
+              alt="React SNS 로고"
+              className="h-6 w-6"
             />
-            <div className="font-bold">한입 로그</div>
+            <span
+              className="
+                text-[15px]
+                font-semibold
+                tracking-tight
+                text-slate-800
+                dark:text-slate-100
+              "
+            >
+              React SNS
+            </span>
           </Link>
+
           <div className="flex items-center gap-5">
             <ThemeButton />
             <ProfileButton />
           </div>
         </div>
       </header>
+
       <main className="m-auto w-full max-w-175 flex-1 border-x px-4 py-6">
         <Outlet />
       </main>
+
       <footer className="text-muted-foreground border-t py-10 text-center">
-        @winterlood
+        @KIM YE EUN
       </footer>
     </div>
   );
